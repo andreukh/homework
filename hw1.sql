65a6653ca859a9d139890fa1ee9f7b84494250b9
AndreuKh <andreukh@gmail.com>
2021-12-27 20:55:53 +0300

    add 2nd sql stud

---
+++ hw1.sql
@@ -0,0 +1,7 @@
+--Вывести все поля и все строки
+select * from students s; 
+--Вывести всех студентов в таблице
+select * from students s2; 
+--Вывести только Id пользователей
+select id from students s3;
+
