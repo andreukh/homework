c689a833c23324eb82b37c12b4c1be7be7a58c53
AndreuKh <andreukh@gmail.com>
2021-12-29 17:20:30 +0300

    update sql2

--- hw2.sql
+++ hw2.sql
@@ -1,6 +1,12 @@
+--Создать таблицу employees
+--- id. serial,  primary key,
+--- employee_name. Varchar(50), not null
+
 create table employees(
 id serial primary key,
 employee_name varchar(50) not null);
+
+--Наполнить таблицу employee 70 строками.
 
 insert into employees(employee_name)
 values ('Шамиль'),
@@ -73,13 +79,18 @@ values ('Шамиль'),
 		('Ульяна'),
 		('Нина'),
 		('Виктория');
-select * from employees ;
+--select * from employees ;
+
+--Создать таблицу salary
+--- id. Serial  primary key,
+--- monthly_salary. Int, not null
 
 
 create table salary(
 id serial primary key,
 monthly_salary int not null);
 
+--Наполнить таблицу salary 15 строками:
 insert into salary(monthly_salary)
 values (1000),
 		(1100),
@@ -98,12 +109,22 @@ values (1000),
 		(2400),
 		(2500);
 
-select * from salary;
+--select * from salary;
 
+--	Создать таблицу employee_salary
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique
+--- salary_id. Int, not null
+
+	
 create table employee_salary(
 id serial primary key,
 employee_id int not null unique,
 salary_id int not null);
+
+--	Наполнить таблицу employee_salary 40 строками:
+--- в 10 строк из 40 вставить несуществующие employee_id
+
 
 insert into employee_salary(employee_id, salary_id )
 values (17, 10),
@@ -149,13 +170,20 @@ values (17, 10),
 
 select * from employee_salary ;
 
+--Создать таблицу roles
+--- id. Serial  primary key,
+--- role_name. int, not null, unique
+
+
 create table roles(
 id serial primary key,
 role_name int not null unique);
 
+--Поменять тип столба role_name с int на varchar(30)
 
 alter table roles alter column role_name type varchar(30);
 
+--Наполнить таблицу roles 20 строками:
 insert into roles(role_name)
 values ('Junior Python developer'),
 		('Middle Python developer'),
@@ -177,8 +205,13 @@ values ('Junior Python developer'),
 		('Junior Automation QA engineer'),
 		('Middle Automation QA engineer'),
 		('Senior Automation QA engineer');
-		
-select * from roles;
+--		
+--select * from roles;
+
+--Создать таблицу roles_employee
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+--- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
 
 create table roles_employee(
 id serial primary key,
@@ -188,6 +221,8 @@ foreign key(employee_id)
 references employees(id),
 foreign key(role_id)
 references roles(id));
+
+--Наполнить таблицу roles_employee 40 строками:
 
 insert into roles_employee (employee_id, role_id)
 values (55, 17),
